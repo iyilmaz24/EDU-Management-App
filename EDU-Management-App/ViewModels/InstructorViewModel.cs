839725cae6c838385251d7a6b9d9c36ff70514f2
Irfan Yilmaz <iy.usa@outlook.com>
2024-03-14 21:44:30 -0400

    begin connecting VM to the M (MVVM)

--- EDU-Management-App/ViewModels/InstructorViewModel.cs
+++ EDU-Management-App/ViewModels/InstructorViewModel.cs
@@ -1,6 +1,34 @@
 namespace EDU_Management_App.ViewModels;
 
+using EDU_App_Library.Services;
+using EDU_App_Library.Models;
 public class InstructorViewModel
 {
+    private CourseService courseService;
+    private StudentService studentService;
+    public InstructorViewModel(string UserId)
+    {
+        courseService = CourseService.Current;
+        studentService = StudentService.Current;
+    }
+
+    // adds new student to general roster of students
+    public void AddStudent(string name, string classI) {
+        Student newStu = new Student(name, classI);
+        studentService.AddStudent(newStu);
+    }
+
+    // enrolls existing student to a specific course
+    public void EnrollStudent(Student student, Course course)
+    {
+        courseService.AddStudent(student, course);
+    }
+    
+    // return the entire general roster of students
+    public IList<Student> GetAllStudents()
+    {
+        return studentService.students;
+    }
     
 }
+
