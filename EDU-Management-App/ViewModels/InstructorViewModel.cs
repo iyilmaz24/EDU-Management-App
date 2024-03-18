77ef9e430226d59c6e7ac68adea29e564f06b775
Irfan Yilmaz <iy.usa@outlook.com>
2024-03-18 04:04:50 -0400

    begin adding student from View UI to VM

--- EDU-Management-App/ViewModels/InstructorViewModel.cs
+++ EDU-Management-App/ViewModels/InstructorViewModel.cs
@@ -9,12 +9,12 @@ using EDU_App_Library.Models;
 public class InstructorViewModel : INotifyPropertyChanged
 {
     public event PropertyChangedEventHandler PropertyChanged;
-
     private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
     {
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
-
+    
+    public Student SelectedPerson { get; set; }
     public ObservableCollection<Student> StudentsList
     {
         get => new ObservableCollection<Student>(StudentService.Current.students); 
@@ -23,8 +23,8 @@ public class InstructorViewModel : INotifyPropertyChanged
     public InstructorViewModel()
     {
         AddStudent("John Smith", "2nd Year");
-        AddStudent("Ana Jones", "4th Year");
-        AddStudent("Michael Yellow", "3rd Year");
+        // AddStudent("Ana Jones", "4th Year");
+        // AddStudent("Michael Yellow", "3rd Year");
     }
 
     // adds new student to general roster of students
@@ -34,17 +34,17 @@ public class InstructorViewModel : INotifyPropertyChanged
         NotifyPropertyChanged(nameof(StudentsList));
     }
 
-    // enrolls existing student to a specific course
-    public void EnrollStudent(Student student, Course course)
-    {
-        CourseService.Current.AddStudent(student, course);
-    }
-    
-    // return the entire general roster of students
-    public ObservableCollection<Student> GetAllStudents()
-    {
-        return new ObservableCollection<Student>(StudentService.Current.students);
-    }
+    // // enrolls existing student to a specific course
+    // public void EnrollStudent(Student student, Course course)
+    // {
+    //     CourseService.Current.AddStudent(student, course);
+    // }
+    //
+    // // return the entire general roster of students
+    // public ObservableCollection<Student> GetAllStudents()
+    // {
+    //     return new ObservableCollection<Student>(StudentService.Current.students);
+    // }
 
 }
 
